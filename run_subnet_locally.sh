4fc346d0b11ffd618699f64cea303de4ed53e0fb
Pawel Kanarek <paw.kanarek@gmail.com>
2024-03-05 00:45:14 +0100

    add activate conda function

--- run_subnet_locally.sh
+++ run_subnet_locally.sh
@@ -3,7 +3,7 @@
 # bit tensor docs https://github.com/opentensor/bittensor#install
 
 subtensor_dir='subtensor'
-bittensor_env='bittensor'
+conda_env='bittensor'
 
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
@@ -18,49 +18,8 @@ print_error() {
     echo -e "${RED}--- $1${NC}"
 }
 
-# ---- Conda stuff 
-if ! command -v conda &> /dev/null; then
-    print "Conda is not installed. Installing..."
-    # Download and install Miniconda
-    mkdir -p $HOME/miniconda3
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda3/miniconda.sh
-    bash $HOME/miniconda3/miniconda.sh -b -u -p $HOME/miniconda3/
-    rm $HOME/miniconda3/miniconda.sh
-    $HOME/miniconda3/bin/conda init bash
-    print "Conda is installed."
-    print "Restart bash with command source ~/.bashrc, and run this script again"
-    exec bash
-    exit 1
-fi
-
-if ! command -v conda &> /dev/null; then
-    print_error "Cannot find conda. If problem persist, install conda manually."
-    exit 1
-fi
-
-
-print "Conda init" 
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)" # without this I get strange errors when activating conda envs
-print "Current conda env is ($CONDA_DEFAULT_ENV)"
-
-if conda info --envs | grep -q "\b$bittensor_env\b"; then
-    print "Conda environment '$bittensor_env' already exists."
-else
-    print "Creating conda environment $bittensor_env ..."
-    conda create -n $bittensor_env python=3.10
-
-    git clone git@github.com:opentensor/bittensor.git
-    python3 -m pip install -e bittensor/
-fi
-
-print "Activate conda env $bittensor_env ..."
-conda activate $bittensor_env
-print "current env is $CONDA_DEFAULT_ENV"
-
-if [ "$CONDA_DEFAULT_ENV" != "$bittensor_env" ]; then
-    print "Conda did not activate correct enviroment. Exiting."
-    exit 1 
-fi
+# CONDA 
+activate_conda $conda_env
 
 
 # https://docs.docker.com/desktop/install/ubuntu/
--- start_subnet.sh
+++ start_subnet.sh
@@ -3,6 +3,8 @@ GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+conda_env='bittensor'
+
 
 print() {
     echo -e "${CYAN}--- $1${NC}"
@@ -16,48 +18,7 @@ print_error() {
 
 cd subtenosr
 
-# ---- Conda stuff 
-if ! command -v conda &> /dev/null; then
-    print "Conda is not installed. Installing..."
-    # Download and install Miniconda
-    mkdir -p $HOME/miniconda3
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda3/miniconda.sh
-    bash $HOME/miniconda3/miniconda.sh -b -u -p $HOME/miniconda3/
-    rm $HOME/miniconda3/miniconda.sh
-    $HOME/miniconda3/bin/conda init bash
-    print "Conda is installed."
-    print "Restart bash with command source ~/.bashrc, and run this script again"
-    exec bash
-    exit 1
-fi
-
-if ! command -v conda &> /dev/null; then
-    print_error "Cannot find conda. If problem persist, install conda manually."
-    exit 1
-fi
-
-print "Conda init" 
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)" # without this I get strange errors when activating conda envs
-print "Current conda env is ($CONDA_DEFAULT_ENV)"
-
-if conda info --envs | grep -q "\b$bittensor_env\b"; then
-    print "Conda environment '$bittensor_env' already exists."
-else
-    print "Creating conda environment $bittensor_env ..."
-    conda create -n $bittensor_env python=3.10
-
-    git clone git@github.com:opentensor/bittensor.git
-    python3 -m pip install -e bittensor/
-fi
-
-print "Activate conda env $bittensor_env ..."
-conda activate $bittensor_env
-print "current env is $CONDA_DEFAULT_ENV"
-
-if [ "$CONDA_DEFAULT_ENV" != "$bittensor_env" ]; then
-    print "Conda did not activate correct enviroment. Exiting."
-    exit 1 
-fi
+activate_conda $conda_env
 
 main(){
     
@@ -82,9 +43,9 @@ main(){
             break
         fi
         # SKEETCHY
-        sleep 15
+        sleep 30
         COLD_KEY_PASS
-        sleep 15
+        sleep 30
         counter=$((counter+1))
     done
     
