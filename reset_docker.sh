4e2a95bb17c774e12d21e0e62c664575f506dddf
Pawel Kanarek <paw.kanarek@gmail.com>
2024-03-05 20:26:19 +0100

    update reset docker script

--- reset_docker.sh
+++ reset_docker.sh
@@ -4,7 +4,7 @@ source activate_conda.sh
 
 activate_conda "bittensor"
 
-print "Remove all containers"
+print "docker rm $(docker ps -a -q)"
 if [ "$(docker ps -a -q)" ]; then
     docker stop $(docker ps -a -q)
     docker rm $(docker ps -a -q)
@@ -12,21 +12,28 @@ else
     print "No containers to remove"
 fi
 
-
-print "Remove all images"
+print "docker rmi $(docker images -a -q)"
 if [ "$(docker images -a -q)" ]; then
     docker rmi $(docker images -a -q)
 else
     print "No images to remove"
 fi
 
-print "Remove all volumes"
+print "docker volume rm $(docker volume ls -q)"
 if [ "$(docker volume ls -q)" ]; then
     docker volume rm $(docker volume ls -q)
 else
     print "No volumes to remove"
 fi
 
-
-print "Remove networks"
+print "docker network prune"
 docker network prune
+
+print "docker container prune"
+docker container prune
+
+print "docker volume prune"
+docker volume prune
+
+print "docker system prune --all --volumes --force"
+docker system prune --all --volumes --force
