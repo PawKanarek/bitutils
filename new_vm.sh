53cad5e30c5119bdb9ab2c4c93e2b4b7a00f4406
Pawel Kanarek <paw.kanarek@gmail.com>
2024-03-07 11:30:23 +0000

    added hack for xla problems on tpu

--- new_vm.sh
+++ new_vm.sh
@@ -3,3 +3,6 @@ ssh-add ~/.ssh/id_ed25519
 conda activate tpu 
 git config --global user.email paw.kanarek@gmail.com
 git config --global user.name "Pawel Kanarek"
+# Fix for tpu xla
+find / -name "libpython3.10.so.1.0" 2>/dev/null
+export LD_LIBRARY_PATH=/home/raix/miniconda3/envs/tpu/lib:$LD_LIBRARY_PATH
