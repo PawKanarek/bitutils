c1f52106e50ea6a41be35aed9407793f83c009ab
pawkanarek <paw.kanarek@gmail.com>
2024-04-04 13:06:53 +0200

    Update new_vm.sh

--- new_vm.sh
+++ new_vm.sh
@@ -1,8 +1,10 @@
+ssh-keygen -t ed25519 -C "paw.kanarek@gmail.com"
+
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 conda activate tpu 
 git config --global user.email paw.kanarek@gmail.com
-git config --global user.name "Pawel Kanarek"
+git config --global user.name "PawKanarek"
 # Fix for tpu xla
 find / -name "libpython3.10.so.1.0" 2>/dev/null
 export LD_LIBRARY_PATH=/home/raix/miniconda3/envs/tpu/lib:$LD_LIBRARY_PATH
